1d5de83ec995f8473a37b6f4c9a62f35baeeaaa2
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2024-08-25 08:56:03 -0700

    Add comment for understanding clarity

--- src/spmv/spmv_bsr.cu
+++ src/spmv/spmv_bsr.cu
@@ -116,7 +116,7 @@ __global__ void spmv_bsr_block_kernel(BSRMatrix<T> A, T* x, T* y) {
     unsigned int block_col = A.colIdx[A.rowPtrs[block_row] + local_block_idx];
 
     if(block_row_idx < b) {
-        shared_x[block_row_idx] = x[block_col * b + block_row_idx];
+        shared_x[block_row_idx] = x[block_col * b + block_row_idx]; // here block_row_idx is essentially used for the column indexes of the block (ideally if block dimensions were unequal, we would have to use threadIdx.y for the column index)
     }
     __syncthreads();
 
